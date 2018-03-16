759345cdcf9e9f5c1824262b42fe7becd4804ba9
DaftCreation <daftcreation01@gmail.com>
2018-03-16 17:30:49 +0530

    Scrollbar Animation Glide in Effect

---
+++ JQUERY Main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 5){
+            $("#flour").css("left","560px").css("top","200px");
+            $("#pappers").css("left","550px").css("top","285px");
+            $("#cheese").css("left","550px").css("top","355px");
+            $("#tomato").css("left","500px").css("top","330px");
+            $("#chili").css("left","495px").css("top","255px");
+            $("#gradient").css("left","545px").css("top","255px");
+            $("#peppermint").css("left","525px").css("top","190px");
+            $("#onion").css("left","560px").css("top","145px");
+            $("#garlic").css("left","510px").css("top","270px");
+        } 
+    }); 
+});
+
+
+
+
+
+
+
